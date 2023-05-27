799234db938c5e0b1c0e7b4e761c290fc207089b
miroNever <123352959+miroNever@users.noreply.github.com>
2023-05-27 16:02:23 +0500

    Add files via upload

--- zadanieConsole/Program.cs
+++ zadanieConsole/Program.cs
@@ -66,95 +66,23 @@ namespace zadanieConsole
             return count;
 
         }
-        // задание 2
-        //A
-        static void CheckNum(string text)
-        {
-            IEnumerable<char> str =
-                from c in text
-                where char.IsDigit(c)
-                select c;
-
-            Console.Write("Всего чисел в строке: " + str.Count());
-        }
-        //B
-        static void OutputTo(string text)
-        {
-            IEnumerable<char> str = text.TakeWhile(c => c != '/');
-
-            foreach (char c in str)
-            {
-                Console.Write(c);
-            }
-        }
-        //C
-        static void OutputAfter(string text)
-        {
-            IEnumerable<char> str = text.SkipWhile(c => c != '/');
-
-            string outtext = "";
-
-            foreach (char c in str)
-            {
-                if (c == '/')
-                    continue;
-                else
-                {
-                    if (c >= 'а' && c <= 'я' || c >= 'a' && c <= 'z')
-                        outtext += c.ToString().ToUpper();
-                    else
-                        if (c >= 'А' && c <= 'Я' || c >= 'A' && c <= 'Z')
-                        outtext += c.ToString().ToLower();
-                }
-            }
-
-            Console.WriteLine(outtext);
-        }
+        
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.Write("Введите какое задание хотите проверить 1 задание(1), 2 задание(2): ");
-                try
+                Console.Write("\nВведите слово которое будете искать в файле: ");
+                string word = Console.ReadLine();
+                if (Check1(word))
                 {
-                    int choice = int.Parse(Console.ReadLine());
-                    switch (choice)
-                    {
-                        case 1:
-                            Console.Write("\nВведите слово которое будете искать в файле: ");
-                            string word = Console.ReadLine();
-                            if (Check1(word))
-                            {
-                                string line = "";
-                                line = ReadingFile(line);
-                                int count = Search(word.ToLower(), line.ToLower());
-                                Console.WriteLine($"Были найдены {count} вхождения(ий) поискового запроса {word}\n");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Слово должно быть написано на русском языке.");
-                            }
-
-                            break;
-                        case 2:
-                            Console.WriteLine("Введите строку");
-                            string text = Console.ReadLine();
-                            Console.WriteLine("\n");
-                            CheckNum(text);
-                            Console.WriteLine("\n");
-                            OutputTo(text);
-                            Console.WriteLine("\n");
-                            OutputAfter(text);
-                            Console.WriteLine("\n");
-                            break;
-                        default:
-                            Console.WriteLine("Введите 1 или 2");
-                            break;
-                    }
+                    string line = "";
+                    line = ReadingFile(line);
+                    int count = Search(word.ToLower(), line.ToLower());
+                    Console.WriteLine($"Были найдены {count} вхождения(ий) поискового запроса {word}\n");
                 }
-                catch (Exception)
+                else
                 {
-                    Console.WriteLine("Введи число");
+                    Console.WriteLine("Слово должно быть написано на русском языке.");
                 }
             }
         }
